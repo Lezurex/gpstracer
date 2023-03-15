3660c9ecc249870ade73d50556e49f45519e1099
Lenny Angst <lenny@familie-angst.ch>
2023-03-15 16:01:30 +0100

    feat: Add location indicator

--- app/src/main/java/com/lezurex/gpstracer/MapActivity.kt
+++ app/src/main/java/com/lezurex/gpstracer/MapActivity.kt
@@ -3,6 +3,7 @@ package com.lezurex.gpstracer
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lezurex.gpstracer.util.LocationPermissionHelper
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.eq
@@ -10,15 +11,32 @@ import com.mapbox.maps.extension.style.expressions.generated.Expression.Companio
 import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.literal
 import com.mapbox.maps.extension.style.layers.addLayer
 import com.mapbox.maps.extension.style.layers.generated.FillExtrusionLayer
-
-var mapView: MapView? = null
+import com.mapbox.maps.plugin.locationcomponent.location
+import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS) { style ->
+
+        locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
+        locationPermissionHelper.checkPermissions {
+            onMapReady()
+        }
+    }
+
+    private fun onMapReady() {
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { style ->
+            mapView.location.updateSettings {
+                enabled = true
+                pulsingEnabled = true
+            }
             setupBuildings(style)
         }
     }
@@ -36,4 +54,5 @@ class MainActivity : AppCompatActivity() {
         fillExtrusionLayer.fillExtrusionAmbientOcclusionRadius(3.0)
         style.addLayer(fillExtrusionLayer)
     }
+
 }
