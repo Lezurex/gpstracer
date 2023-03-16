7dd62f0ee9580c7c1304e8133b833bc40d1f7a23
Lenny Angst <lenny@familie-angst.ch>
2023-03-16 08:27:37 +0100

    refactor: remove manual extrusion layer from map 3D buildings (and terrain) are now included in the custom style. Therefore, the manual extrusion layer isn't needed anymore.

--- app/src/main/java/com/lezurex/gpstracer/MapActivity.kt
+++ app/src/main/java/com/lezurex/gpstracer/MapActivity.kt
@@ -1,16 +1,9 @@
 package com.lezurex.gpstracer
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lezurex.gpstracer.util.LocationPermissionHelper
 import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.eq
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.get
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.literal
-import com.mapbox.maps.extension.style.layers.addLayer
-import com.mapbox.maps.extension.style.layers.generated.FillExtrusionLayer
 import com.mapbox.maps.plugin.locationcomponent.location
 import java.lang.ref.WeakReference
 
@@ -32,27 +25,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onMapReady() {
-        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { style ->
+        mapView.getMapboxMap().loadStyleUri("mapbox://styles/lezurex/clf9trxio00d201q6frjc01tu") {
             mapView.location.updateSettings {
                 enabled = true
                 pulsingEnabled = true
             }
-            setupBuildings(style)
         }
     }
-
-    private fun setupBuildings(style: Style) {
-        val fillExtrusionLayer = FillExtrusionLayer("3d-buildings", "composite")
-        fillExtrusionLayer.sourceLayer("building")
-        fillExtrusionLayer.filter(eq(get("extrude"), literal("true")))
-        fillExtrusionLayer.minZoom(10.0)
-        fillExtrusionLayer.fillExtrusionColor(Color.parseColor("#aaaaaa"))
-        fillExtrusionLayer.fillExtrusionHeight(get("height"))
-        fillExtrusionLayer.fillExtrusionBase(get("min_height"))
-        fillExtrusionLayer.fillExtrusionOpacity(0.6)
-        fillExtrusionLayer.fillExtrusionAmbientOcclusionIntensity(0.3)
-        fillExtrusionLayer.fillExtrusionAmbientOcclusionRadius(3.0)
-        style.addLayer(fillExtrusionLayer)
-    }
-
 }
